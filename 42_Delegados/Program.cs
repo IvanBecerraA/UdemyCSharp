d8a8b3185395408f4f22da5064b14521ff65ad9c
IvanBecerraA <ivanbecerra91@gmail.com>
2025-12-11 22:24:54 -0300

    add: 44.- Delegado genérico Action

--- 42_Delegados/Program.cs
+++ 42_Delegados/Program.cs
@@ -1,21 +1,33 @@
 ﻿Operation mySum = Functions.Sum;
-Console.WriteLine(mySum(3, 5));
+//Console.WriteLine(mySum(3, 5));
 
 mySum = Functions.Mul;
-Console.WriteLine(mySum(3, 5));
+//Console.WriteLine(mySum(3, 5));
 
 Show show = Functions.ConsoleShow;
-show("Hola mundo!");
+//show("Hola mundo!");
 
 Show cw = Console.WriteLine;
 cw += Functions.ConsoleShow; // Multi difucion
 //cw("Hola mundo2!");
 
-Functions.Some("Iván", "Becerra", cw);
+//Functions.Some("Iván", "Becerra", cw);
 
+#region Action
+
+Action<string> showMessage = Console.WriteLine;
+
+Functions.SomeAction("Iván", "Becerra", showMessage);
+
+#endregion
+
+
+#region Delegados
 delegate int Operation(int a, int b);
 public delegate void Show(string message);
-
+public delegate void Show2(string message, string message2);
+public delegate void Show3(string message, string message2, string message3);
+#endregion
 
 public class Functions
 {
@@ -25,10 +37,12 @@ public class Functions
 
     public static void ConsoleShow(string m) => Console.WriteLine(m.ToUpper());
 
-    public static void Some(string name, string lastName, Show fn)
+    public static void SomeAction(string name, string lastName, Action<string> fn)
     {
         Console.WriteLine("Hago algo al inicio");
         fn($"Hola {name} {lastName}");
         Console.WriteLine("Hago algo al final");
     }
+
+
 }
